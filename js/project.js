10384e42f7fce97a098b9a62bccf9d0fd4ad540e
13circle <13circle97@gmail.com>
2020-07-15 20:13:17 +0900

    Javascript files for base html pages
    * script.js: javascript file for all pages
    * project.js: javascript file for projects.html

---
+++ js/project.js
@@ -0,0 +1,61 @@
+(function projectsOnload() {
+    renderProjectList();
+})();
+
+function getReqProject() {
+    return window.location.href.split('?').pop();
+}
+
+function renderProjectList() {
+    var project_dir = './project-info/';
+    var project_list = document.querySelector('.project-list');
+    var xhr, json, a, jsons = [];
+    $.getJSON(project_dir, json_names => {
+        json_names.forEach(json_name => {
+            xhr = getXHR(function() {
+                if(this.readyState == 4) {
+                    if(this.status == 200) {
+                        json = JSON.parse(this.responseText);
+                        jsons.push(json);
+                        if(jsons.length == json_names.length) {
+                            jsons = sortJSONarr(jsons);
+                            jsons.forEach(j => {
+                                a = document.createElement('a');
+                                a.href = j['project-page-link'];
+                                a.className = 'list-group-item';
+                                a.textContent = j['project-title'];
+                                project_list.appendChild(a);
+                                if(j['project-filename'] == getReqProject() + '.json') {
+                                    for(var key in j) {
+                                        var elmnts = document.querySelectorAll('.' + key), txhr;
+                                        var project_detail = document.querySelector('.project-detail');
+                                        if(elmnts.length > 0) {
+                                            elmnts.forEach(elmnt => {
+                                                if(elmnt.href) {
+                                                    if(j[key].length > 0) elmnt.href = j[key];
+                                                    else elmnt.removeAttribute('href');
+                                                }
+                                                elmnt.textContent = (j[key].length > 0) ? j[key] : 'None';
+                                            });
+                                            txhr = getXHR(function() {
+                                                if(this.readyState == 4) {
+                                                    if(this.status == 200) {
+                                                        project_detail.innerHTML = this.responseText.replace(/\n/g, '<br>');
+                                                    }
+                                                }
+                                            });
+                                            txhr.open('GET', './project-details/' + j['project-filename'].replace('.json', '.html'), true);
+                                            txhr.send();
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+                }
+            });
+            xhr.open('GET', project_dir + '/' + json_name, true);
+            xhr.send();
+        });
+    });
+}
---
+++ js/script.js
@@ -0,0 +1,168 @@
+var toTopBtn;
+
+window.onload = function() {
+    renderProjectView();
+    includeHTML('templates', 'template', function() {
+        renderProjectMenu();
+        setScrollToTopBtn();
+        setSmoothAnchorMove();
+        setTooltip();
+
+        toTopBtn = document.querySelector('.to-top');
+    });
+};
+
+function setScrollToTopBtn() {
+    $(window).scroll(function() {
+        if($(window).scrollTop() + $(window).height() > $(document).height() * 0.50) {
+            toTopBtn.style.display = 'block';
+        } else {
+            toTopBtn.style.display = 'none';
+        }
+    });
+}
+
+function setSmoothAnchorMove() {
+    $(document).on('click', 'a[href^="#to"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 500);
+    });
+}
+
+function setTooltip() {
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+}
+
+function getXHR(readyStateChangeCallBack) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    if(readyStateChangeCallBack) xhr.onreadystatechange = readyStateChangeCallBack;
+    return xhr;
+}
+
+function sortJSONarr(jsons) {
+    var sorted_jsons = jsons.slice();
+    sorted_jsons.sort(function(a, b) {
+        var f, l;
+        if(typeof a != 'string') {
+            f = JSON.stringify(a);
+            l = JSON.stringify(b);
+        } else {
+            f = a; l = b;
+        }
+        return f.localeCompare(l);
+    });
+    return sorted_jsons;
+}
+
+function includeHTML(project_dir, attr, initCallback) {
+    var elements, html, xhr;
+    elements = document.querySelectorAll('div, span');
+    for(var i = 0, json, key, elmnt; i < elements.length; i++) {
+        if(html = elements[i].getAttribute(attr)) {
+            xhr = getXHR(function() {
+                if(this.readyState == 4) {
+                    if(this.status == 200) {
+                        elements[i].innerHTML = this.responseText;
+                        if(json = elements[i].getAttribute('my-json-data')) {
+                            json = JSON.parse(json);
+                            for(key in json) {
+                                if(elmnt = elements[i].querySelector('.' + key)) {
+                                    if(elmnt.href) {
+                                        if(json[key].length > 0)
+                                            elmnt.href = json[key];
+                                        else elmnt.textContent = '';
+                                    } else elmnt.textContent = json[key];
+                                }
+                            }
+                        }
+                    }
+                    if(this.status == 404)
+                        elements[i].innerHTML = '404 - Page not found';
+                    elements[i].removeAttribute(attr);
+                    includeHTML(project_dir, attr, initCallback);
+                    if((i == elements.length - 1) && initCallback) initCallback();
+                }
+            });
+            if(html.lastIndexOf('.html') == -1) html += '.html';
+            xhr.open('GET', project_dir + '/' + html, true);
+            xhr.send();
+            return;
+        }
+    }
+}
+
+function renderProjectView() {
+    var project_dir = './project-info/', breakpt = 3;
+    var card_list = document.querySelector('.project-card-list');
+    var row, rows;
+    if(card_list) {
+        row = document.createElement('div');
+        row.className = 'row';
+        card_list.appendChild(row);
+        rows = card_list.querySelectorAll('.row');
+        $.getJSON(project_dir, json_names => {
+            for(var i = 0, xhr, div, jsons = []; i < json_names.length; i++) {
+                xhr = getXHR(function() {
+                    if(this.readyState == 4) {
+                        if(this.status == 200) {
+                            jsons.push(this.responseText);
+                            if(jsons.length == json_names.length) {
+                                jsons = sortJSONarr(jsons);
+                                jsons.forEach((json, json_index) => {
+                                    div = document.createElement('div');
+                                    div.setAttribute('template', 'project-card');
+                                    div.setAttribute('my-json-data', json);
+                                    div.className = 'col-lg-4 mb-4';
+                                    rows[rows.length - 1].innerHTML += div.outerHTML;
+                                    if((json_index + 1) % breakpt == 0) {
+                                        row = document.createElement('div');
+                                        row.className = 'row';
+                                        card_list.appendChild(row);
+                                        rows = card_list.querySelectorAll('.row');
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
+                xhr.open('GET', project_dir + '/' + json_names[i], true);
+                xhr.send();
+            }
+        });
+    }
+}
+
+function renderProjectMenu() {
+    var project_dir = './project-info/';
+    var project_menu = document.querySelector('.project-menu');
+    var xhr, json, a, jsons = [];
+    $.getJSON(project_dir, json_names => {
+        project_menu.innerHTML = '';
+        json_names.forEach(json_file => {
+            xhr = getXHR(function() {
+                if(this.readyState == 4) {
+                    if(this.status == 200) {
+                        jsons.push(JSON.parse(this.responseText));
+                        if(jsons.length == json_names.length) {
+                            jsons = sortJSONarr(jsons);
+                            jsons.forEach(json => {
+                                a = document.createElement('a');
+                                a.className = 'dropdown-item';
+                                a.href = json['project-page-link'];
+                                a.textContent = json['project-title'];
+                                project_menu.appendChild(a);
+                            });
+                        }
+                    }
+                }
+            });
+            xhr.open('GET', project_dir + '/' + json_file, true);
+            xhr.send();
+        });
+    });
+}
